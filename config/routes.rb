e006d6512468f5445043066245a25e6c8527c404
Frank <lebaksari567@gmail.com>
2018-10-30 11:02:34 +0700

    Add Job

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resource  :service
-  resource  :portofolios
+
   get 'service/index'
   get 'portofolios/index'
+  get 'job/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
