7628a85151c8e059e13bc685cfe0185b4f1bb01a
Frank <lebaksari567@gmail.com>
2018-10-31 13:36:58 +0700

    tablist

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,3 +39,17 @@ $(document).ready(function(){
         $('#back-to-top').tooltip('show');
 
 });
+
+function listTab(evt, skillName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(skillName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
